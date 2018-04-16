2c1c3e8d904dc307760648ff0597a2f4f504c049
bmullan-pivotal <mullan@gmail.com>
2018-04-16 08:17:27 -0400

    first

---
+++ src/main/java/com/example/concoursedemo/ConcoursedemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.concoursedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcoursedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcoursedemoApplication.class, args);
+	}
+}
